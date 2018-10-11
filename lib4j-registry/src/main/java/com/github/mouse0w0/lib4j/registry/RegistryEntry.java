ec33a5d3127249c0493e9577b65ef9cdb2c8bd82
Mouse <937279156@qq.com>
2018-10-11 21:49:03 +0800

    Update Registry

--- lib4j-registry/src/main/java/com/github/mouse0w0/lib4j/registry/RegistryEntry.java
+++ lib4j-registry/src/main/java/com/github/mouse0w0/lib4j/registry/RegistryEntry.java
@@ -14,27 +14,27 @@ public interface RegistryEntry<T> {
         return setRegistryName(new NamespacedKey(name));
     }
 
+    default T setRegistryName(String domain, String path) {
+        return setRegistryName(new NamespacedKey(domain, path));
+    }
+
     abstract class Impl<T extends RegistryEntry<T>> implements RegistryEntry<T> {
 
         private final TypeToken<T> token = new TypeToken<T>(getClass()) {
         };
-        private NamespacedKey location;
+        private NamespacedKey key;
 
         @Override
         public final NamespacedKey getRegistryName() {
-            return location;
+            return key;
         }
 
         @SuppressWarnings("unchecked")
         @Override
         public final T setRegistryName(NamespacedKey location) {
-            if (this.location != null) throw new IllegalStateException("Duplicated register");
-            this.location = location;
+            if (this.key != null) throw new IllegalStateException("Duplicated register");
+            this.key = location;
             return (T) this;
-        }
-
-        public final T setRegistryName(String domain, String path) {
-            return setRegistryName(new NamespacedKey(domain, path));
         }
 
         @SuppressWarnings("unchecked")
