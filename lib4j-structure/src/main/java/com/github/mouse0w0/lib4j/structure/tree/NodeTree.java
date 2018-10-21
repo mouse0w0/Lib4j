30355c596f65aab2d43d86462c6741b17a286684
Mouse <937279156@qq.com>
2018-10-21 15:36:36 +0800

    Update Structure

--- lib4j-structure/src/main/java/com/github/mouse0w0/lib4j/structure/tree/NodeTree.java
+++
@@ -1,8 +0,0 @@
-package com.github.mouse0w0.lib4j.structure.tree;
-
-public class NodeTree {
-
-    private class Node {
-
-    }
-}
--- lib4j-structure/src/main/java/com/github/mouse0w0/lib4j/structure/tree/TireTreeImpl.java
+++ lib4j-structure/src/main/java/com/github/mouse0w0/lib4j/structure/tree/TireTreeImpl.java
@@ -4,96 +4,210 @@ import java.util.*;
 
 public class TireTreeImpl implements TireTree {
 
-    private final List<String> elements = new LinkedList<String>() {
-
-        @Override
-        public boolean add(String s) {
-            return super.add(s);
-        }
-
-        @Override
-        public String set(int index, String element) {
-            throw new UnsupportedOperationException();
-        }
-    };
+    private final List<String> elements = new LinkedList<String>();
 
     private final Node root = new Node();
 
     @Override
     public Collection<String> get(String prefix) {
-        return null;
-    }
+        Node node = root;
+        if (prefix == null)
+            return node.get();
 
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
+        for (char c : prefix.toCharArray()) {
+            node = node.children.get(c);
+            if (node == null)
+                return Collections.emptyList();
+        }
+        return node.get();
     }
 
     @Override
     public Iterator<String> iterator() {
-        return null;
+        return new Iterator<String>() {
+            private int index = 0;
+            private String currentElement;
+
+            @Override
+            public boolean hasNext() {
+                return index < elements.size();
+            }
+
+            @Override
+            public String next() {
+                currentElement = elements.get(index);
+                return currentElement;
+            }
+
+            @Override
+            public void remove() {
+                TireTreeImpl.this.remove(currentElement);
+            }
+        };
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return elements.toArray();
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        return elements.toArray(a);
     }
 
     @Override
     public boolean add(String s) {
-        return false;
+        Objects.requireNonNull(s, "Element cannot be null.");
+        int index = 0;
+        for (int size = size(); index < size; index++) {
+            if (s.compareTo(elements.get(index)) <= 0) {
+                break;
+            }
+        }
+        elements.add(index, s);
+        add(index, s);
+        return true;
+    }
+
+    private void add(int index, String s) {
+        Node node = root;
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            boolean flag = true;
+            for (Map.Entry<Character, Node> entry : node.children.entrySet()) {
+                if(entry.getKey().equals(c)) {
+                    node = entry.getValue();
+                    increaseNode(node, index);
+                    flag = false;
+                } else {
+                    increaseNodeAndChildren(entry.getValue(), index);
+                }
+            }
+
+            if(flag) {
+                node = node.children.put(c, new Node(index));
+            }
+        }
+    }
+
+    private void increaseNode(Node node, int index) {
+        if (index < node.from) { // if index before the node.
+            node.from++;
+        }
+
+        if (index <= node.to) { // if index before or in the node.
+            node.to++;
+        }
+    }
+
+    private void increaseNodeAndChildren(Node node, int index) {
+        if(index > node.to) {
+            return;
+        }
+
+        increaseNode(node, index);
+        for(Node child : node.children.values())
+            increaseNodeAndChildren(child, index);
     }
 
     @Override
     public boolean remove(Object o) {
+        for (int i = 0, size = size(); i < size; i++) {
+            String s = elements.get(i);
+            if (s.equals(o)) {
+                elements.remove(i);
+                decreaseNodeAndChildren(root, i);
+                return true;
+            }
+        }
         return false;
+    }
+
+    private void decreaseNode(Node node, int index) {
+        if (index < node.from) { // if index before the node.
+            node.from--;
+        }
+
+        if (index <= node.to) { // if index before or in the node.
+            node.to--;
+        }
+    }
+
+    private void decreaseNodeAndChildren(Node node, int index) {
+        if(index > node.to) {
+            return;
+        }
+
+        for(Map.Entry<Character, Node> entry : node.children.entrySet()) {
+            Node child = entry.getValue();
+            decreaseNodeAndChildren(child, index);
+            if(child.to - child.from < 1) {
+                node.children.remove(entry.getKey());
+            }
+        }
+        decreaseNode(node, index);
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        return elements.containsAll(c);
     }
 
     @Override
     public boolean addAll(Collection<? extends String> c) {
-        return false;
+        boolean flag = true;
+        for (String s : c) {
+            if (!add(s)) {
+                flag = false;
+            }
+        }
+        return flag;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        boolean flag = true;
+        for (Object o : c) {
+            if (!remove(o)) {
+                flag = false;
+            }
+        }
+        return flag;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        return elements.retainAll(c);
     }
 
     @Override
     public void clear() {
+        elements.clear();
+        root.clear();
+    }
 
+    @Override
+    public int size() {
+        return elements.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return elements.contains(o);
     }
 
     private class Node {
         private int from, to;
-        private final Map<Character, Node> childrens = new HashMap<>();
+        private final Map<Character, Node> children = new HashMap<>();
 
-        public Node() {}
+        public Node() {
+        }
 
         public Node(int index) {
             this(index, index + 1);
@@ -102,6 +216,15 @@ public class TireTreeImpl implements TireTree {
         public Node(int from, int to) {
             this.from = from;
             this.to = to;
+        }
+
+        public void set(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public void clear() {
+            children.clear();
         }
 
         public Collection<String> get() {
