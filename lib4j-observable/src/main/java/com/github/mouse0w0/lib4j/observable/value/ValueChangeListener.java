d96cd6260496406e440192dfe2efc2514d52858e
Mouse <mouse0w0@qq.com>
2019-01-05 23:17:40 +0800

    Fix a error

--- lib4j-observable/src/main/java/com/github/mouse0w0/lib4j/observable/value/ValueChangeListener.java
+++ lib4j-observable/src/main/java/com/github/mouse0w0/lib4j/observable/value/ValueChangeListener.java
@@ -3,5 +3,5 @@ package com.github.mouse0w0.lib4j.observable.value;
 @FunctionalInterface
 public interface ValueChangeListener<T> {
 
-    void onChanged(ObservableValue<T> observable, T oldValue, T newValue);
+    void onChanged(ObservableValue<? extends T> observable, T oldValue, T newValue);
 }
